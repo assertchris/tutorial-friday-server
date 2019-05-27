28c67543b924d028396849ede4d3edbdf336df1f
Christopher Pitt <cgpitt@gmail.com>
2019-05-27 19:57:36 +0200

    Use views

--- app/http/controllers/HomeController.py
+++ app/http/controllers/HomeController.py
@@ -1,10 +1,13 @@
 """A HomeController Module."""
 
 from masonite.request import Request
+from masonite.response import Response
 from masonite.view import View
 
 class HomeController:
     """HomeController Controller Class."""
 
-    def show(self, view: View, request: Request):
-        return 'hello ' + (request.param('name') or request.input('name'))
+    def show(self, view: View, request: Request, response: Response):
+        return view.render('home', {
+            'name': request.param('name') or request.input('name'),
+        })
