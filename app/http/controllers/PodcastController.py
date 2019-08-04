aceeedce7303ba69b6743520e91609c9042a9cc5
Christopher Pitt <cgpitt@gmail.com>
2019-08-04 20:05:55 +0200

    Start making podcast list dynamic

--- app/http/controllers/PodcastController.py
+++ app/http/controllers/PodcastController.py
@@ -1,5 +1,11 @@
 from masonite.view import View
 
+
 class PodcastController:
     def show_search(self, view: View):
-        return view.render('podcasts.search')
+        return view.render('podcasts.search', {
+            'podcasts': self.get_podcasts()
+        })
+
+    def get_podcasts(self, query=''):
+        return []
